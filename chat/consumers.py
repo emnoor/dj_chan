e3d21f0c2e5c6b9668a087d4e2ad1781828d0c95
Enam Mijbah Noor <enammijbahnoor@gmail.com>
2023-06-17 15:18:07 +0600

    remove prints

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,8 +6,6 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-        print(f"connect: {self.room_name=} {self.channel_name=}")
-
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
@@ -15,13 +13,11 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive_json(self, content, **kwargs):
-        print(f"receive: {content=} {self.channel_name=}")
         message = content["message"]
         await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat_message", "message": message}
         )
 
     async def chat_message(self, event):
-        print(f"chat_message: {event=} {self.channel_name=}")
         message = event["message"]
         await self.send_json({"message": message})
